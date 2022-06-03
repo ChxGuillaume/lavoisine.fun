b302de5e3b54a61ab63f892936ede072dcd665e5
Guillaume Chx <guillaume.chateauroux@gmail.com>
2022-06-03 02:04:51 +0200

    Initial Commit

---
+++ nuxt.config.js
@@ -0,0 +1,89 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    titleTemplate: 'LaVoisine.fun',
+    title: 'LaVoisine.fun',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: 'og:title',
+        content: 'LaVoisine.fun',
+      },
+      {
+        name: 'og:description',
+        content: `
+          Well done Julien!
+          You're a real pro!
+        `,
+      },
+      {
+        name: 'og:image',
+        content: 'https://www.lavoisine.fun/confetti-512.png',
+      },
+      { name: 'theme-color', content: '#ffe266' },
+    ],
+    link: [{ rel: 'icon', type: 'image/png', href: '/confetti-16.png' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,220 @@
+<template>
+  <div>
+    <v-card
+      id="well-done"
+      v-intersect="goTo"
+      class="screen-card d-flex flex-column justify-center"
+      color="transparent"
+      elevation="0"
+    >
+      <v-avatar
+        id="toggleConfetti"
+        size="128"
+        rounded
+        class="mx-auto"
+        :class="iconClasses"
+        @click="toggleConfetti"
+      >
+        <v-img v-if="iconStatus" src="/confetti-512.png"/>
+        <v-img v-else src="/confetti-off-512.png"/>
+      </v-avatar>
+      <v-card-title class="d-block my-5 text-center text-h2">Good job Julien!</v-card-title>
+      <v-card-subtitle class="d-block text-center text-h4">
+        You succeeded with <span class="font-italic">la voisine
+      </span>.
+      </v-card-subtitle>
+      <canvas id="confetti" ref="confetti"/>
+    </v-card>
+    <v-card
+      id="statistics"
+      v-intersect="goTo"
+      class="screen-card d-flex flex-column justify-center"
+      color="dark-gray"
+      elevation="0"
+    >
+      <v-card-title class="d-block text-center text-h2">Yeyyyyy!</v-card-title>
+      <v-card-subtitle class="d-block text-center text-h4">Time for some Statistics</v-card-subtitle>
+    </v-card>
+    <v-card
+      id="footer"
+      v-intersect="goTo"
+      class="screen-card d-flex flex-column justify-center"
+      color="transparent"
+      elevation="0"
+    >
+      <v-card-title class="d-block text-center text-h2">Hey!</v-card-title>
+      <v-card-subtitle class="d-block text-center text-h4">I don't know why i made this.</v-card-subtitle>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import anime from 'animejs/lib/anime.es.js';
+
+const confetti = require('canvas-confetti');
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+export default {
+  data() {
+    return {
+      confetti: null,
+      confettiColors: [
+        '#78eac4',
+        '#f7c4db',
+        '#ef8bbd',
+        '#cab4ef',
+        '#a183e2',
+        '#ffa585',
+        '#8bb6ef',
+      ],
+      currentPage: 'well-done',
+      showConfetti: true,
+      toggleAnimation: null,
+      animationStatus: false,
+      iconStatus: true,
+    }
+  },
+  computed: {
+    iconClasses() {
+      return this.animationStatus ? '' : ['pointer', 'transition'];
+    },
+  },
+  mounted() {
+    this.confetti = confetti.create(this.$refs.confetti, {resize: true, useWorker: true});
+
+    this.confettiLeft();
+
+    this.toggleAnimation = anime.timeline({
+      duration: 800,
+      ease: 'easeInOutSine',
+      autoplay: false,
+    });
+
+    let changedIcon = false;
+    this.toggleAnimation.add({
+      targets: '#toggleConfetti',
+      rotate: '1turn',
+      scale: [
+        {value: 1.5, duration: 300},
+        {value: 1, duration: 300},
+      ],
+      begin: () => {
+        changedIcon = false;
+        this.animationStatus = true;
+      },
+      complete: () => {
+        this.animationStatus = false;
+      },
+      update: (anim) => {
+        if (anim.progress > 10 && !changedIcon) {
+          changedIcon = true;
+          this.iconStatus = !this.iconStatus;
+          this.showConfetti = this.iconStatus;
+          this.confettiLeft();
+        }
+      },
+    });
+  },
+  beforeDestroy() {
+    this.showConfetti = false;
+  },
+  methods: {
+    toggleConfetti() {
+      this.toggleAnimation.restart();
+    },
+    confettiLeft() {
+
+      if (this.showConfetti && this.currentPage === 'well-done') {
+        this.confetti({
+          angle: randomInRange(30, 90),
+          colors: this.confettiColors,
+          spread: 100,
+          particleCount: Math.max(window.innerWidth / 2560 * 150, 100),
+          startVelocity: Math.max(window.innerWidth / 2560 * 75, 20),
+          origin: {x: 0, y: Math.max(window.innerWidth / 2560 * 0.6, 0.2)},
+          ticks: 300,
+        })
+
+        setTimeout(() => window.requestAnimationFrame(this.confettiRight), Math.random() * 1000 + 125);
+      }
+    },
+    confettiRight() {
+      if (this.showConfetti && this.currentPage === 'well-done') {
+        this.confetti({
+          angle: randomInRange(180 - 30, 180 - 90),
+          colors: this.confettiColors,
+          spread: 100,
+          particleCount: Math.max(window.innerWidth / 2560 * 150, 100),
+          startVelocity: Math.max(window.innerWidth / 2560 * 75, 20),
+          origin: {x: 1, y: Math.max(window.innerWidth / 2560 * 0.6, 0.2)},
+          ticks: 300,
+        })
+
+        setTimeout(() => window.requestAnimationFrame(this.confettiLeft), Math.random() * 1000 + 125);
+      }
+    },
+    goTo(entries) {
+      if (entries[0].isIntersecting) {
+        this.currentPage = entries[0].target.id;
+
+        this.$vuetify.goTo(`#${this.currentPage}`, {
+          duration: 500,
+          easing: 'easeInOutCubic'
+        }).then(() => {
+          if (entries[0].target.id === 'well-done') {
+            this.confettiLeft();
+          }
+        });
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+#confetti {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  pointer-events: none;
+}
+
+.transition {
+  transition: .2s ease;
+}
+
+.pointer {
+  cursor: pointer;
+}
+
+.pointer:hover {
+  transform: scale(0.8) !important;
+}
+
+.screen-card {
+  position: relative;
+
+  width: 100vw;
+  min-height: 100vh;
+
+  z-index: 1;
+}
+
+.screen-card:first-child {
+  margin-bottom: 50px;
+}
+
+.screen-card:not(:first-child):not(:last-child) {
+  margin: 50px 0;
+}
+
+.screen-card:last-child {
+  margin-top: 50px;
+}
+</style>
